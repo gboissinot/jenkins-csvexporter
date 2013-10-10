d72b3ffbe5ad5a01382f6e2e714c5a42bc0767fa
Gregory Boissinot <gregory.boissinot@gmail.com>
2013-10-10 18:36:47 +0200

    Restore remote mode

--- csvexporter-batch/src/main/java/com/boissinot/jenkins/csvexporter/launcher/Main.java
+++ csvexporter-batch/src/main/java/com/boissinot/jenkins/csvexporter/launcher/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) throws JobInstanceAlreadyCompleteException, JobParametersInvalidException, JobRestartException, JobExecutionAlreadyRunningException {
 
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext();
-        applicationContext.getEnvironment().setActiveProfiles("local");
+        applicationContext.getEnvironment().setActiveProfiles("remote");
         applicationContext.setConfigLocation("applicationContext-csv.xml");
         applicationContext.refresh();
 
