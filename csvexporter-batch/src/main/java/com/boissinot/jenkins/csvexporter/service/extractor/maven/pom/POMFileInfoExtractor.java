26ee853a883b7f61e92ff9272f124d1f5e1b5292
Gregory Boissinot <gregory.boissinot@gmail.com>
2013-10-11 19:23:01 +0200

    Add check null

--- csvexporter-batch/src/main/java/com/boissinot/jenkins/csvexporter/service/extractor/maven/pom/POMFileInfoExtractor.java
+++ csvexporter-batch/src/main/java/com/boissinot/jenkins/csvexporter/service/extractor/maven/pom/POMFileInfoExtractor.java
@@ -42,11 +42,13 @@ public class POMFileInfoExtractor {
         if (configJob.getCvsModule() != null) {
             String cvsModule = configJob.getCvsModule();
             final Map<String, String> cvsMap = contextModuleMap.get("CVS");
-            String modulePath = cvsMap.get(cvsModule) == null ? cvsModule : cvsMap.get(cvsModule);
-            if (configJob.getCvsBranche() != null) {
-                return new POMRemoteObj(configJob.getJobName(), csvViewerRootUrl + modulePath + "/pom.xml?revision=" + configJob.getCvsBranche());
-            } else {
-                return new POMRemoteObj(configJob.getJobName(), csvViewerRootUrl + modulePath + "/pom.xml?view=co");
+            if (cvsMap != null) {
+                String modulePath = cvsMap.get(cvsModule) == null ? cvsModule : cvsMap.get(cvsModule);
+                if (configJob.getCvsBranche() != null) {
+                    return new POMRemoteObj(configJob.getJobName(), csvViewerRootUrl + modulePath + "/pom.xml?revision=" + configJob.getCvsBranche());
+                } else {
+                    return new POMRemoteObj(configJob.getJobName(), csvViewerRootUrl + modulePath + "/pom.xml?view=co");
+                }
             }
         }
 
