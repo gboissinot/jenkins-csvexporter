37969875bdd132cac8e7deff862a94a389ab27de
Gregory Boissinot <gregory.boissinot@gmail.com>
2013-10-18 10:59:56 +0200

    Test with Interception

--- csvexporter-batch/src/main/java/com/boissinot/jenkins/csvexporter/integration/EmailMapViewer.java
+++ csvexporter-batch/src/main/java/com/boissinot/jenkins/csvexporter/integration/EmailMapViewer.java
@@ -1,11 +1,36 @@
 package com.boissinot.jenkins.csvexporter.integration;
 
 import com.boissinot.jenkins.csvexporter.domain.OutputCSVJobObj;
+import org.springframework.integration.Message;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.channel.ChannelInterceptor;
+import org.springframework.integration.channel.interceptor.ChannelInterceptorAdapter;
 
 /**
  * @author Gregory Boissinot
  */
-public class EmailMapViewer {
+public class EmailMapViewer extends ChannelInterceptorAdapter{
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        return super.preSend(message, channel);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
+        super.postSend(message, channel, sent);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Message<?> postReceive(Message<?> message, MessageChannel channel) {
+        return super.postReceive(message, channel);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public boolean preReceive(MessageChannel channel) {
+        System.out.println("TOTO");
+        return true;
+    }
 
     public void display(OutputCSVJobObj outputCSVJobObj) {
         System.out.println(outputCSVJobObj.getName() + "-->" + outputCSVJobObj.getDevelopers());
