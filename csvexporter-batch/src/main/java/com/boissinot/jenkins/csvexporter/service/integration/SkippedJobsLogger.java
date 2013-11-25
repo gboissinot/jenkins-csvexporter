f49a1a8ea6dedd8fbc0d16723346c96c7bfe091b
Gregory Boissinot <gregory.boissinot@gmail.com>
2013-11-25 09:47:57 +0100

    Move to constant

--- csvexporter-batch/src/main/java/com/boissinot/jenkins/csvexporter/service/integration/SkippedJobsLogger.java
+++ csvexporter-batch/src/main/java/com/boissinot/jenkins/csvexporter/service/integration/SkippedJobsLogger.java
@@ -1,5 +1,6 @@
 package com.boissinot.jenkins.csvexporter.service.integration;
 
+import com.boissinot.jenkins.csvexporter.domain.JobMessageHeaders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.integration.annotation.Header;
@@ -13,7 +14,7 @@ public class SkippedJobsLogger {
     private static Logger logger = LoggerFactory.getLogger(SkippedJobsLogger.class);
 
     @ServiceActivator
-    public void log(@Header("JOB_NAME") String skippedJobName) {
+    public void log(@Header(JobMessageHeaders.HEADER_JOB_NAME) String skippedJobName) {
         logger.info(String.format("Skipping '%s'", skippedJobName));
     }
 }
