03c4db25878c0bfed029f3e3f897910b41570a5a
Gregory Boissinot <gregory.boissinot@gmail.com>
2013-11-25 10:42:06 +0100

    Remove InitializingBean

--- csvexporter-batch/src/main/java/com/boissinot/jenkins/csvexporter/batch/CleanupOutputFilesTasklet.java
+++ csvexporter-batch/src/main/java/com/boissinot/jenkins/csvexporter/batch/CleanupOutputFilesTasklet.java
@@ -4,8 +4,6 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.util.Assert;
 
 import java.io.File;
 import java.util.List;
@@ -13,17 +11,15 @@ import java.util.List;
 /**
  * @author Gregory Boissinot
  */
-public class CleanupOutputFilesTasklet implements Tasklet, InitializingBean {
+public class CleanupOutputFilesTasklet implements Tasklet {
 
     private final List<String> filePathList2Delete;
 
     public CleanupOutputFilesTasklet(List<String> filePathList2Delete) {
+        if (filePathList2Delete == null) {
+            throw new NullPointerException("A list of file path for deletion must be set");
+        }
         this.filePathList2Delete = filePathList2Delete;
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        Assert.notNull(filePathList2Delete, "A list of file path for deletion must be set");
     }
 
     @Override
